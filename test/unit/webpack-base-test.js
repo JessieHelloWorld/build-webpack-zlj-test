70107ef89b4acf2f5c2faca328b688c0ec97cab8
zhoujessie <zhouljeasc@163.com>
2019-07-20 21:44:49 +0800

    test: webpack.base test

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -3,7 +3,7 @@ const assert = require('assert')
 describe('webpack.base.js test case', () => {
   const baseConfig = require('../../lib/webpack.base.js')
   it('entry', () => {
-    assert.equal(baseConfig.entry.index, '/Users/jessiezhou/code/webpack_study/geek_time_clf_webpack/builder-webpack/test/smoke/template/src/index/index.js')
-    assert.equal(baseConfig.entry.search, '/Users/jessiezhou/code/webpack_study/geek_time_clf_webpack/builder-webpack/test/smoke/template/src/search/index.js')
+    assert.equal(baseConfig.entry.index.indexOf('builder-webpack/test/smoke/template/src/index/index.js') > -1, true)
+    assert.equal(baseConfig.entry.search.indexOf('builder-webpack/test/smoke/template/src/search/index.js') > -1, true)
   })
 })
